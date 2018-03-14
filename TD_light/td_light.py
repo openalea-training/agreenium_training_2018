8262e8a5afb9852795ea20f54447e76b05c8334b
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2018-03-15 00:08:10 +0100

    update TD light

--- TD_light/td_light.py
+++ TD_light/td_light.py
@@ -1,8 +1,12 @@
 import pandas
+import numpy
+import matplotlib.pyplot as plt
 import openalea.plantgl.all as pgl
-from alinea.astk.sun_and_sky import sun_sky_sources, sky_sources
+from alinea.astk.sun_and_sky import sun_sky_sources, sky_sources, \
+    cie_relative_luminance
 from alinea.caribu.CaribuScene import CaribuScene
 from alinea.caribu.light import light_sources
+from visual_objects import mtg_tree, mtg_nenuphar
 
 
 def read_meteo_mpt(when='winter'):
@@ -29,10 +33,36 @@ def test_scene(s):
                 print(pid)
 
 
-
-
 def illuminate(scene):
     light = light_sources(*sky_sources())
     cs = CaribuScene(scene, light=light,scene_unit='cm')
     raw, agg = cs.run(direct=True, simplify=True)
     return cs, raw, agg
+
+
+def light_response(irradiance, alpha=0.01, Pm=1, R=0):
+    return alpha * irradiance * Pm  / (alpha * irradiance + Pm) -R
+
+
+def irrad_opt(alpha=0.01, Pm=1):
+    return 0.7 * Pm / alpha / (1 - 0.7)
+
+def net_photosynthesis(ei):
+    pass
+
+
+def polar_grid(zenith=20, azimuth=80):
+    """ generate elevation, azimuth and zenith matrix for polar plot"""
+
+    zen, az = numpy.meshgrid(numpy.radians(numpy.linspace(0,90,zenith)),
+                              numpy.radians(numpy.linspace(0,360,azimuth)))
+    return numpy.radians(90) - zen, az, zen
+
+
+def polar_plot(zen, az, values):
+    fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
+    m=ax.contourf(az, zen, values)
+    plt.colorbar(m)
+    plt.show()
+
+
--- TD_light/visual_objects.py
+++ TD_light/visual_objects.py
@@ -75,3 +75,45 @@ def display_leaves(g):
         scene.add(sh)
     pgl.Viewer.display(scene)
     return scene
+
+def mtg_nenuphar(area=1):
+    g = MTG()
+    vid = g.add_component(g.root, label= 'plant', edge_type='/')
+    geom = pgl.Disc(numpy.sqrt(area / 0.9 / numpy.pi))
+    g.add_child(vid, edge_type='<', label='trunk', area=area, geometry=geom)
+    return g
+
+def cartesian(elevation, azimuth, radius=1):
+    """ Cartesian coordinates from elevation and azimuth angles"""
+    zenith = numpy.radians(90) - elevation
+    x = radius * numpy.sin(zenith) * numpy.cos(azimuth)
+    y = radius * numpy.sin(zenith) * numpy.sin(azimuth)
+    z = radius * numpy.cos(zenith)
+    return zip(x,y,z)
+
+def add_sun(scene, elevation, azimuth, distance=5, radius=0.1):
+    sph = pgl.Sphere(radius)
+    red = pgl.Material(pgl.Color3(150, 0, 0))
+    pos = cartesian(elevation, azimuth, distance)
+    pgl_pos = [pgl.Vector3(*p) for p in pos]
+    for vect in pgl_pos:
+        shape = pgl.Translated(vect, sph)
+        scene.add(pgl.Shape(shape,red))
+    return scene
+
+def add_sky(scene, distance=4, radius= 0.05):
+    sph = pgl.Sphere(radius)
+    blue = pgl.Material(pgl.Color3(0, 0, 150))
+    elevations46 = [9.23] * 10 + [10.81] * 5 + [26.57] * 5 + [31.08] * 10 + [
+                    47.41] * 5 + [52.62] * 5 + [69.16] * 5 + [90]
+    azimuths46 = [12.23, 59.77, 84.23, 131.77, 156.23, 203.77, 228.23, 275.77,
+                  300.23, 347.77, 36, 108, 180, 252, 324, 0, 72, 144, 216, 288,
+                  23.27, 48.73, 95.27, 120.73, 167.27, 192.73, 239.27, 264.73,
+                  311.27, 336.73, 0, 72, 144, 216, 288, 36, 108, 180, 252, 324,
+                  0, 72, 144, 216, 288, 180]
+    pos = cartesian(numpy.radians(elevations46), numpy.radians(azimuths46), distance)
+    pgl_pos = [pgl.Vector3(*p) for p in pos]
+    for vect in pgl_pos:
+        shape = pgl.Translated(vect, sph)
+        scene.add(pgl.Shape(shape,blue))
+    return scene
